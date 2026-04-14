5818769bf8365b088fc5ecd0b2416254cbf2f1dd
Swapnil Ingle <ingleswapnil2004@gmail.com>
2026-04-14 16:07:12 +0530

    feat: Add filesystem plugin support and enhance capabilities for file operations

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -94,6 +94,7 @@ fn discover_server() -> Result<Vec<ServerInfo>, String> {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_clipboard_manager::init())
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_shell::init())
